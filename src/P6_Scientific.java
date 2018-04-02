36a99b10501faf77358e59ef9560aec64eeb6c86
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 20:49:33 -0500

    Update P6_Scientific.java

--- src/P6_Scientific.java
+++ src/P6_Scientific.java
@@ -1,14 +1,29 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/1/2018
 */
 
 public class P6_Scientific {
 
 	public static void main(String[] args) {
-		// REMINDER: Any print statements should only be in the main method
-
+		
+		computeScientificValue(6.5,3);
 	}
 
+	
+	
+	public static double computeScientificValue(double base, double exp) {
+	     
+		double power; 
+		power = Math.pow(10, exp);
+		double answer;
+		answer = (base*power);
+		return answer; 
+		
+		
+		
+  }
+	
+	
 }
