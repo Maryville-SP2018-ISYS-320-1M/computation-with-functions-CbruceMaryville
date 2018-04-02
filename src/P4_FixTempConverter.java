fec334b4ac85254ba584dcc49b5a42017c7e2eac
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 19:38:19 -0500

    Update P4_FixTempConverter.java

--- src/P4_FixTempConverter.java
+++ src/P4_FixTempConverter.java
@@ -1,12 +1,13 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/1/2018
 */
 
 /*
  What were the errors you found?
- 
+ The call in the main class for printing tempc was using the 0.0 method.
+ You need to output the tempc inside the Temp converter
  
  */
 
@@ -15,11 +16,12 @@ public class P4_FixTempConverter {
 		double tempf = 98.6;
 		double tempc = 0.0;
 		ftoc(tempf, tempc);
-		System.out.println("Body temp in C is: " + tempc);
+		//System.out.println("Body temp in C is: " + tempc);
 	}
 
 	// converts Fahrenheit temperatures to Celsius
 	public static void ftoc(double tempf, double tempc) {
 	      tempc = (tempf - 32) * 5 / 9;
+	      System.out.println(tempc);
     }
 }
